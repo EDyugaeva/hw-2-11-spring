81cb2d47009353d6429d1c3b830e2514c81d7989
EDyugaeva <ed2408@yandex.ru>
2022-02-16 21:17:08 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hw211spring/Hw211SpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw211spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw211SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hw211spring/Hw211SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw211spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
